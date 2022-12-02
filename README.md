b82db328ce8c8aab01306d92b178cd24cae5bcfe
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2022-12-02 00:26:28 -0500

    solve part one - largest sum of sum array

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# aoc-22
-Advent of Code - 2022
+# Advent of Code - 2022
+
+My git repository for the 2022 Advent of Code challenge. I'll be using Java
+this year.
---
+++ src/AdventOfCode/DayOne/CalorieCounting.java
@@ -0,0 +1,37 @@
+// Alexander Jenkins
+// 2022-12-01
+
+package AdventOfCode.DayOne;
+
+// import files
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CalorieCounting {
+    public static void main(String[] args) {
+        // read the data into memory
+        try {
+            File elves = new File("./src/AdventOfCode/DayOne/dayOne.txt");
+            Scanner r = new Scanner(elves);
+       
+            // go through the file
+            int mostCals = 0;
+            int tempSum = 0;
+            while (r.hasNextLine()) {
+                var token = r.nextLine();
+                if (token.equals("")) {
+                    mostCals = tempSum > mostCals ? tempSum : mostCals;
+                    tempSum = 0;
+                    continue;
+                }
+                tempSum += Integer.parseInt(token);
+            }
+            System.out.printf("One elf has %d calories available!", mostCals);
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured when opening the file");
+            e.printStackTrace();
+        }
+
+    }
+}
