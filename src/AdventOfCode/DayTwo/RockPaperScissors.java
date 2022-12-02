81a0caf371f29512ce7731014fce35ba87c70d84
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2022-12-02 14:04:33 -0500

    start day 2

--- src/AdventOfCode/DayTwo/RockPaperScissors.java
+++ src/AdventOfCode/DayTwo/RockPaperScissors.java
@@ -1,6 +1,8 @@
 // Alexander Jenkins
 // 2022-12-02
 
+package AdventOfCode.DayTwo;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -12,40 +14,15 @@ public class RockPaperScissors {
             Scanner r = new Scanner(rounds);
 
             
+            while (r.hasNextLine()) {
+                var line = r.nextLine();
+                System.out.println(line);
+            }
 
         } catch (FileNotFoundException e) {
             System.out.println("Encountered and error opening the file.");
             e.printStackTrace();
         }
-
-    }
-
-    enum Round = {
-        ROCK (1), // 'a' or 'x'
-        PAPER (2), // 'b' or 'y'
-        SCISSORS (3), // 'c' or 'z'
-        LOSS (0),
-        DRAW (3),
-        WIN (6);
-
-        private final char playerOne;
-        private final char playerTwo;
-
-        Round(char p1, char p2) {
-            playerOne = p1;
-            playerTwo = p2;
-        }
-
-        private String roundResult(Round r) {
-            return switch(String.format("%s%s", r.playerOne, r.playerTwo)) {
-                case "AX", "BY", "CZ" -> DRAW;
-                case "CX", "YA", "BZ" -> WIN;
-                default -> LOSS;
-            }
-        }
-
-        int value() {
-            return matchResult() + 
 
     }
 
